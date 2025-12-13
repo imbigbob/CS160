3512dab985dedeaa70493026a2e7fc647abc843b
SpheraXX <haigr2004@gmail.com>
2025-12-13 15:00:10 +0700

    fix some conflict

--- main.cpp
+++ main.cpp
@@ -5,15 +5,6 @@
 #include <iostream>
 #include <string>
 
-#include "src/Program/Program.hpp"
+// #include "src/Program/Program.hpp"
 
-int main() {
-    try {
-        Program program;
-        program.run();
-    } catch (std::exception& e) {
-        std::cout << "EXCEPTION bug: " << e.what() << '\n';
-    }
-
-    return 0;
-}
+int main() { return 0; }
