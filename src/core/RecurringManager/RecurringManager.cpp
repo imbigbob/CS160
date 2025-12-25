c654d431786c8f9d0819c29b11b1c0f116e084dd
SpheraXX <binbo242@gmail.com>
2025-12-25 08:40:36 +0700

    addRule function

--- src/core/RecurringManager/RecurringManager.cpp
+++ src/core/RecurringManager/RecurringManager.cpp
@@ -1,7 +1,24 @@
 #include "RecurringManager.hpp"
-void RecurringManager::addRule(const RecurringTransaction& rule) {}
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <json/json.h>
 
-void RecurringManager::applyForCurrentMonth(TransactionManager& tm) {
-    // Placeholder implementation
-    return;
+#include "model/Income/Income.hpp"
+#include "model/Expense/Expense.hpp"
+
+void RecurringManager::addRule(const RecurringTransaction& rule) {
+    switch(rule.type) {
+        case 1: {
+            incomeRules.pushBack(rule);
+            updateDB(incomeFilepath, incomeRules);
+        }
+        case 2: {
+            expenseRules.pushBack(rule);
+            updateDB(incomeFilepath, expenseRules);           
+        }
+    }
 }
+
