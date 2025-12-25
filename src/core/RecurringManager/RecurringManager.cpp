adb0a4a990ea923d249c4e536ba7ebe554ed3356
SpheraXX <binbo242@gmail.com>
2025-12-25 09:23:52 +0700

    include loadFromDB function

--- src/core/RecurringManager/RecurringManager.cpp
+++ src/core/RecurringManager/RecurringManager.cpp
@@ -9,7 +9,19 @@
 #include "model/Income/Income.hpp"
 #include "model/Expense/Expense.hpp"
 
+// File path
+RecurringManager::RecurringManager() {
+    this->incomeFilepath = "data/RecurringIncomes.json";
+    this->expenseFilepath = "data/RecurringExpenses.json";
+
+    loadFromDB(incomeFilepath, incomeRules, 1);
+    loadFromDB(expenseFilepath, expenseRules, 2);
+}
+
+// Rule adding
 void RecurringManager::addRule(const RecurringTransaction& rule) {
+
+    // Adding rules
     switch(rule.type) {
         case 1: {
             incomeRules.pushBack(rule);
@@ -21,4 +33,49 @@ void RecurringManager::addRule(const RecurringTransaction& rule) {
         }
     }
 }
+
+// Current year month with format YYYY-MM
+std::string RecurringManager::getCurrentYM() {
+
+    // ctime
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    
+    // stringstream lib
+    std::stringstream ss;
+    ss << (now->tm_year + 1900) << "-" 
+       << std::setw(2) << std::setfill('0') << (now->tm_mon + 1); // iomanip
+    return ss.str();
+}
+
+void RecurringManager::loadFromDB(const std::string& filepath, DynamicArray<RecurringTransaction>& list, int typeTransaction) {
+
+    // Json read stuff
+    std::ifstream file(filepath);
+    if (!file.is_open()) return;
+
+    Json::Value root;
+    Json::CharReaderBuilder reader;
+    std::string errs;
+
+    if (!Json::parseFromStream(reader, file, &root, &errs)) return;
+
+    for (const auto& obj : root) {
+
+        // Define type of transaction
+        RecurringTransaction r;
+        r.type = typeTransaction; 
+
+        if (obj.isMember("amount")) r.amount = obj["amount"].asDouble();
+        if (obj.isMember("categoryId")) r.categoryId = obj["categoryId"].asString();
+        if (obj.isMember("categoryName")) r.categoryName = obj["categoryName"].asString();
+        if (obj.isMember("walletId")) r.walletId = obj["walletId"].asString();
+        if (obj.isMember("description")) r.description = obj["description"].asString();
+        if (obj.isMember("day")) r.day = obj["day"].asInt();
+        if (obj.isMember("lastAppliedYM")) r.lastAppliedYM = obj["lastAppliedYM"].asString();
+
+        list.pushBack(r);
+    }
+}
+
 
